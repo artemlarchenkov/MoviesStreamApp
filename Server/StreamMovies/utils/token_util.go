902602ea3db8caa426b5c66b435db2cbf8a1e8ef
artemlarchenkov <112898231+artemlarchenkov@users.noreply.github.com>
2026-06-26 14:45:10 +0300

    feat: Работа с входом пользователей

--- Server/StreamMovies/utils/token_util.go
+++ Server/StreamMovies/utils/token_util.go
@@ -1,1 +1,37 @@
 package utils
+
+import (
+	"os"
+	"time"
+
+	"github.com/artemlarchenkov/MoviesStreamApp/Server/StreamMovies/database"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type SignedDetatils struct {
+	Email     string
+	FirstName string
+	LastName  string
+	Role      string
+	UserId    string
+	jwt.RegisteredClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email, firstName, lastName, role, userId string) (string, string, error) {
+	claims := &SignedDetatils{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		UserID:    userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "MagicStream",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+}
