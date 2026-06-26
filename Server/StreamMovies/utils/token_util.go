420e74f3eb71ef64c4153a967f422a969085098a
artemlarchenkov <112898231+artemlarchenkov@users.noreply.github.com>
2026-06-26 15:25:03 +0300

    feat: added functions to generate access token and refresh token

--- Server/StreamMovies/utils/token_util.go
+++ Server/StreamMovies/utils/token_util.go
@@ -4,10 +4,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/artemlarchenkov/MoviesStreamApp/Server/StreamMovies/database"
 	jwt "github.com/golang-jwt/jwt/v5"
-	"go.mongodb.org/mongo-driver/v2/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type SignedDetatils struct {
@@ -20,6 +17,7 @@ type SignedDetatils struct {
 }
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
+var SECRET_REFRESH_KEY string = os.Getenv("SECRET_REFRESH_KEY")
 
 func GenerateAllTokens(email, firstName, lastName, role, userId string) (string, string, error) {
 	claims := &SignedDetatils{
@@ -27,11 +25,38 @@ func GenerateAllTokens(email, firstName, lastName, role, userId string) (string,
 		FirstName: firstName,
 		LastName:  lastName,
 		Role:      role,
-		UserID:    userId,
+		UserId:    userId,
 		RegisteredClaims: jwt.RegisteredClaims{
 			Issuer:    "MagicStream",
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
 		},
 	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, error := token.SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		return "", "", error
+	}
+
+	refreshClaims := &SignedDetatils{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+		Role:      role,
+		UserId:    userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "MagicStream",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	signedRefreshToken, error := token.SignedString([]byte(SECRET_REFRESH_KEY))
+
+	if err != nil {
+		return "", "", error
+	}
+
+	return signedToken, signedRefreshToken, nil
 }
